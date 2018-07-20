d456e64bd9b1ec5132a2e11355f00c3bc26a0fc7
Charles Galvin <charles@c2dev.com.au>
2018-07-20 10:46:34 +1000

    Adding hello world stuff

---
+++ hello_world/index.js
@@ -0,0 +1,14 @@
+const veryBadWord = "bad word";
+let re = new RegExp(veryBadWord, "gi");
+let myString = "Hello@World, Node.js Stringsss".replace("@", " ").replace(/ss$/gi, "");
+let badWord = "123 3 bad word 123 123";
+if (badWord.indexOf(veryBadWord) >= 0) {
+    console.log("Found a match");
+    badWord = badWord.replace(re, "*****")
+}
+
+let mistype = "Spwlling";
+console.log("Hello world");
+console.log(myString);
+console.log(badWord);
+console.log(mistype);
